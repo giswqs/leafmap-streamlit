9d924a79125d4caff4bfcffa26161cadc1b70d15
Qiusheng Wu <giswqs@gmail.com>
2021-08-07 22:47:48 -0400

    Reordered maps

--- app.py
+++ app.py
@@ -7,22 +7,30 @@ import streamlit as st
 "# leafmap streamlit demo"
 st.markdown('Source code: <https://github.com/giswqs/leafmap-streamlit/blob/master/app.py>')
 
-"## Create a heat map"
+"## Create a 3D map using Kepler.gl"
 with st.echo():
     import streamlit as st
+    import leafmap.kepler as leafmap
+
+    m = leafmap.Map(center=[37.7621, -122.4143], zoom=12)
+    in_csv = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_data.csv'
+    config = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_config.json'
+    m.add_csv(in_csv, layer_name="hex_data", config=config)
+    m.to_streamlit()
+
+
+"## Create a heat map"
+with st.echo():
     import leafmap.foliumap as leafmap
 
     filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-
     m = leafmap.Map(tiles='stamentoner')
     m.add_heatmap(filepath, latitude="latitude", longitude='longitude', value="pop_max", name="Heat map", radius=20)
     m.to_streamlit(width=700, height=500, add_layer_control=True)
     
 
-
 "## Load a GeoJSON file"
 with st.echo():
-
 
     m = leafmap.Map(center=[0, 0], zoom=2)
     in_geojson = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/cable-geo.geojson'
@@ -49,12 +57,3 @@ with st.echo():
     m.to_streamlit()
 
 
-"## Create a 3D map using Kepler.gl"
-with st.echo():
-    import leafmap.kepler as leafmap
-
-    m = leafmap.Map(center=[37.7621, -122.4143], zoom=12)
-    in_csv = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_data.csv'
-    config = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/hex_config.json'
-    m.add_csv(in_csv, layer_name="hex_data", config=config)
-    m.to_streamlit()
